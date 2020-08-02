64c565a450354797964659efcd8980f6f8f68816
Saiharsha B <saibalsu@gmail.com>
2020-08-02 11:57:43 +0530

    web: add violence detection :sparkles:

---
+++ web/pages/violence.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <PageHead subtitle="Detection & Tracking" title="Violence Detection" />
+    <Card class="has-table">
+      <ViolenceTable data-url="https://coders-of-blaviken-api.herokuapp.com/api/violence/" />
+    </Card>
+  </div>
+</template>
+
+<script>
+import ViolenceTable from '@/components/ViolenceTable';
+import PageHead from '@/components/PageHead';
+import Card from '@/components/Card';
+export default {
+  layout: 'dashboard',
+  components: {
+    PageHead,
+    Card,
+    ViolenceTable,
+  },
+};
+</script>
