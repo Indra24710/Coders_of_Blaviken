bb785f26760cd68b1d57dd9a54f6bfe3ad127e10
Saiharsha B <saibalsu@gmail.com>
2020-08-02 14:48:27 +0530

    web: add footer component :sparkles:

---
+++ web/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer is-fullwidth">
+    <div class="content has-text-centered">
+      <p><strong>Made with ❤️</strong> by Coders of Blaviken.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style>
+.footer {
+  bottom: 0;
+  left: 0;
+}
+</style>
